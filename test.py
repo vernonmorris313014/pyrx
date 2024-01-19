a5bd91a0c8d709fea3696a04f4cbad5ec0073a5f
vernonmorris313014 <156785242+vernonmorris313014@users.noreply.github.com>
2024-01-19 14:14:50 +0600

    Update test.py

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ import time
 from multiprocessing import Process, Queue
 
 
-pool_host = 'monerop.com'
-pool_port = 4242
-pool_pass = 'xx'
-wallet_address = '42ydzcV2cJxGzKpmZMbsmtahZUhprCspgP9pBYoDk5F5HGUYTCBY3Sn83zTcm5yYeVPYsvikWim35ZajZqo5ReBPFg2CF79'
+pool_host = 'xmr-asia1.nanopool.org'
+pool_port = 10300
+pool_pass = 'raiyan088'
+wallet_address = '84AbPm2mCiBCh182gsvqSRXLpEc9JgUJ96x3KQ6h35ECEtSzMWFDamMdWL98pW16tf61vJiw34nYfMii8hTW3pbTDC7BqTG'
 nicehash = False
 
 
