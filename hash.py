4ee4ec1a65deddf78500c312901022595e0f97b3
vernonmorris313014 <156785242+vernonmorris313014@users.noreply.github.com>
2024-01-19 23:43:36 +0600

    Update hash.py

--- hash.py
+++ hash.py
@@ -13,15 +13,13 @@ start = current_milli_time()
 
 x = 0
 
-print(current_milli_time() - start)
-
 while(current_milli_time() - start < 30000):
     m = "Hello RandomX {}".format(x)
-    print("Hashing: {}".format(x))
+    # print("Hashing: {}".format(x))
     h = x
     x += 1
     bh = pyrx.get_rx_hash(m, seed_hash, h)
     hh = binascii.hexlify(bh).decode()
-    print("Result: {}".format(hh))
+    # print("Result: {}".format(hh))
 
-print("Hashing: {}".format(x))
+print("Hashing: {}".format(round(x/30)))
