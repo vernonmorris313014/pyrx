8453795c1f8dd7e39b438b5a6b58595d54a83a38
vernonmorris313014 <156785242+vernonmorris313014@users.noreply.github.com>
2024-01-20 10:46:09 +0600

    Update hash.py

--- hash.py
+++ hash.py
@@ -3,6 +3,6 @@ import pycryptonight
 
 for x in range(2000):
     m = "Hello RandomX {}".format(x)
-    bh = pycryptonight.cn_slow_hash(m, 4, 0, 1)
+    bh = pycryptonight.cn_fast_hash(m)
     hh = binascii.hexlify(bh).decode()
     # print("Result: {}".format(hh))
